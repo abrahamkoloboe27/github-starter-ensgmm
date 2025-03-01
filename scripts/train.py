2a554c1d3121af3a031cfa6e78fd290f596b64f0
Abraham KOLOBOE <abrahamkoloboe2000@gmail.com>
2025-03-01 15:50:53 +0100

    Add a function to plot sales_evolution plot_ventes

--- scripts/train.py
+++ scripts/train.py
@@ -1,5 +1,15 @@
 import pandas as pd
+import plotly.express as px
 def get_data(path):
     df = pd.read_csv(path)
     return df
+
+def plot_ventes(df):
+    fig = px.bar(
+        df,
+        x="Date",
+        y="Total Ventes",
+        title = "Évolution des ventes au cours du temps"
+    )
+    fig.show()
     
